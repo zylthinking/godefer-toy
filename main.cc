2b51dbf39682bace3b744edf3cf1def01b496f27
zyl <zylthinking@users.noreply.github.com>
2018-01-22 11:44:22 +0800

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,46 +1,18 @@
 
-#include <setjmp.h>
+#include "defer.h"
 #include <stdio.h>
 
-struct defer_t {
-    jmp_buf env1;
-    jmp_buf env2;
-    long rsp;
-
-    ~defer_t() {
-        int n = setjmp(env2);
-        asm("mov %%rsp, %0\n" :"=m"(rsp) ::);
-        //printf("save rsp %p %p %p\n", (void *) rsp, this, __builtin_return_address(0));
-
-        if (n == 0) {
-            longjmp(env1, 1);
-        }
-    }
-};
-
-#define defer2(l, ...) \
-({ \
-    defer_t* np = &defer_node_##l; \
-    if (1 == setjmp(np->env1)) { \
-        __asm__ __volatile ("mov %0, %%rsp\n" : :"m"(np->rsp) :"rsp"); \
-        __VA_ARGS__; \
-        longjmp(np->env2, 1); \
-    } \
-    np; \
-})
-#define defer1(l, ...) defer_t defer_node_##l, *dn_ptr_##l = defer2(l, ##__VA_ARGS__)
-#define defer(...) defer1(__line__, ##__VA_ARGS__)
-
-int tests()
+static int tests()
 {
-    struct a{
+    struct a
+    {
         ~a() {
             printf("hello world\n");
         }
     };
 
     int var = 9;
-    if (var == 9) {
+    if (1) {
         puts("normal 1");
         defer(delete (new a));
     }
